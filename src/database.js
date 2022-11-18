df5dc769afa284cf14b018e94d0220f8f8d9ccb1
José David Ayala Franco <73260715+DavidFranco3@users.noreply.github.com>
2022-11-17 18:16:41 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 // Credenciales para conexion local a la BD
-//const URI = "mongodb://localhost:27017/erp-Fredher";
-const URI = "mongodb://127.0.0.1/erp-Fredher";
+// const URI = "mongodb://localhost:27017/erp-Fredher";
+// const URI = "mongodb://127.0.0.1/erp-Fredher";
 
 // Credenciales para conexion a Mongo Atlas
-// const URI = "mongodb+srv://erpFredher:A0KB3nEK33q53xCQ@cluster0.dsbmu.mongodb.net/erp-Fredher";
+const URI = "mongodb+srv://erpFredher:A0KB3nEK33q53xCQ@cluster0.dsbmu.mongodb.net/erp-Fredher";
 
 mongoose.connect(URI)
     .then(db => console.log("DB is connected"))
