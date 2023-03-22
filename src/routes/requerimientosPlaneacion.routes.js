8356eddfa53bdb0c23b65a8a37bb9c2e228d62df
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-03-22 16:23:30 -0600

    correcciones

--- src/routes/requerimientosPlaneacion.routes.js
+++ src/routes/requerimientosPlaneacion.routes.js
@@ -12,7 +12,7 @@ router.post("/registro", async (req, res) => {
     const busqueda = await requerimientosPlaneacion.findOne({ folio });
 
     if (busqueda && busqueda.folio === folio) {
-        return res.status(401).json({ mensaje: "Ya existe un requerimiento con este folio" });
+        return res.status(401).json({ mensaje: "Ya existe una planeación con este folio" });
     } else {
         const requerimiento = requerimientosPlaneacion(req.body);
         await requerimiento
@@ -34,6 +34,17 @@ router.get("/listar", async (req, res) => {
 
     await requerimientosPlaneacion
         .find({ sucursal })
+        .sort({ _id: -1 })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Obtener todos las compras
+router.get("/listarPorSemana", async (req, res) => {
+    const { sucursal, semana } = req.query;
+
+    await requerimientosPlaneacion
+        .find({ sucursal, semana })
         .sort({ _id: -1 })
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
@@ -100,12 +111,12 @@ router.get("/obtenerDatosRequerimiento/:folio", async (req, res) => {
 router.get("/obtenerNoRequerimiento", async (req, res) => {
     const requerimiento = await requerimientosPlaneacion.find().count();
     if (requerimiento === 0) {
-        res.status(200).json({ noRequerimiento: "REQ-1" })
+        res.status(200).json({ noRequerimiento: "PLA-1" })
     } else {
         const ultimoRequerimiento = await requerimientosPlaneacion.findOne().sort({ _id: -1 });
         const tempFolio1 = ultimoRequerimiento.folio.split("-")
         const tempFolio = parseInt(tempFolio1[1]) + 1;
-        res.status(200).json({ noRequerimiento: "REQ-" + tempFolio.toString().padStart(1, 0) })
+        res.status(200).json({ noRequerimiento: "PLA-" + tempFolio.toString().padStart(1, 0) })
     }
 });
 
@@ -130,7 +141,7 @@ router.delete("/eliminar/:id", async (req, res) => {
     const { id } = req.params;
     await requerimientosPlaneacion
         .deleteOne({ _id: id })
-        .then((data) => res.status(200).json({ mensaje: "Requerimiento eliminado" }))
+        .then((data) => res.status(200).json({ mensaje: "Planeación eliminada" }))
         .catch((error) => res.json({ message: error }));
 });
 
@@ -144,13 +155,123 @@ router.put("/actualizarEstado/:id", async (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoLT1/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoLT1 == "true" ? "Se habilito el primer turno del lunes" : "Se inhabilito el primer turno del lunes" }))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoLT2/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoLT2 == "true" ? "Se habilito el segundo turno del lunes" : "Se inhabilito el segundo turno del lunes"}))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoMT1/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoMT1 == "true" ? "Se habilito el primer turno del martes" : "Se inhabilito el primer turno del martes" }))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoMT2/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoMT2 == "true" ? "Se habilito el segundo turno del martes" : "Se inhabilito el segundo turno del martes"}))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoMIT1/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoMIT1 == "true" ? "Se habilito el primer turno del miercoles" : "Se inhabilito el primer turno del miercoles"}))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoMIT2/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoMIT2 == "true" ? "Se habilito el segundo turno del miercoles" : "Se inhabilito el segundo turno del miercoles"}))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoJT1/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoJT1 == "true" ? "Se habilito el primer turno del jueves" : "Se inhabilito el primer turno del jueves" }))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoJT2/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoJT2 == "true" ? "Se habilito el segundo turno del jueves" : "Se inhabilito el segundo turno del jueves"}))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoVT1/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoVT1 == "true" ? "Se habilito el primer turno del viernes" : "Se inhabilito el primer turno del viernes"}))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoVT2/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoVT2 == "true" ? "Se habilito el segundo turno del viernes" : "Se inhabilito el segundo turno del viernes"}))
+        .catch((error) => res.json({ message: error }));
+});
+
+// Para cambiar el estado de la compra
+router.put("/actualizarEstadoST1/:id", async (req, res) => {
+    const { id } = req.params;
+    const { programa } = req.body;
+    await requerimientosPlaneacion
+        .updateOne({ _id: id }, { $set: { programa } })
+        .then((data) => res.status(200).json({ mensaje: programa.estadoST1 == "true" ? "Se habilito el primer turno del sabado" : "Se inhabilito el primer turno del sabado"}))
+        .catch((error) => res.json({ message: error }));
+});
+
 // Actualizar datos de orden de compra
 router.put("/actualizar/:id", async (req, res) => {
     const { id } = req.params;
-    const { requerimiento, planeacion, bom, datosRequisicion } = req.body;
+    const { requerimiento, acumulado, acumuladoMaterial, planeacion, bom, datosRequisicion, resultados, materiaPrima, observaciones } = req.body;
     await requerimientosPlaneacion
-        .updateOne({ _id: id }, { $set: { requerimiento, planeacion, bom, datosRequisicion } })
-        .then((data) => res.status(200).json({ mensaje: "Informacion del requerimiento actualizada", datos: data }))
+        .updateOne({ _id: id }, { $set: { requerimiento, acumulado, acumuladoMaterial, planeacion, bom, datosRequisicion, resultados, materiaPrima, observaciones } })
+        .then((data) => res.status(200).json({ mensaje: "Informacion de la planeación actualizada", datos: data }))
         .catch((error) => res.json({ message: error }));
 });
 
